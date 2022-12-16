a942c6795790f95cfdbc7b8eaea5dcae54d18961
ruzs <eddie10913@gmail.com>
2022-12-16 11:52:11 +0800

    使用繼承，在建構式中更改成員內容 & 在繼承的子類別中增加新的方法

--- index.php
+++ index.php
@@ -1,55 +1,20 @@
 <?php
 
 
-/* 成員的使用
- $cat=new Animal;
-
-$dog=new Animal;
-
-echo $cat->type;
-echo "<br>";
-echo $dog->name; */
-
-/* 權限為public時，可以對成員做修改
- $cat->type='snake';
-$dog->name='mack';
-echo "<br>";
-echo $cat->type;
-echo "<br>";
-echo $dog->name; */
-
-/* echo "<pre>";
-var_dump($cat);
-echo "</pre>";
- */
-
-//方法的使用
-
-/* $cat=new Animal;
-$dog=new Animal;
-
-$cat->run();
-echo $cat->type;
-$cat->speed(); */
-
-
-/* 建構式的使用
-*/
-$cat =new Animal('小花','黑白相間');
+$cat=new Cat('tama','white');
+echo $cat->getType();
 echo $cat->getName();
 echo $cat->getColor();
+$cat->hide();
+echo "<br>";
 
-$dog =new Animal('小莉','土黃色');
+$dog=new Dog('bochi','brown');
+echo $dog->getType();
 echo $dog->getName();
 echo $dog->getColor();
+$dog->eat();
+echo "<br>";
 
-$turtle =new Animal('達文西','墨綠色');
-echo $turtle->getName();
-echo $turtle->getColor();
-
-$guineapig =new Animal('puipui','黃色');
-echo $guineapig->getName();
-echo $guineapig->getColor();
 class Animal{
     protected $type='animal';
     protected $name='John';
@@ -60,6 +25,7 @@ class Animal{
         //$this->run();
         $this->name=$name;
         $this->hair_color=$color;
+        // $this->type=$type;
 
     }
 
@@ -71,7 +37,11 @@ class Animal{
     {
         return $this->hair_color;
     }
-
+    public function getType()
+    {
+        return $this->type;
+    }
+    
     public function run()
     {
         echo "我會跑哦";
@@ -84,7 +54,28 @@ class Animal{
     }
 
 }
-
-
-
+class Cat extends Animal{
+    public function __construct($name,$color)
+    {
+        parent::__construct($name,$color);
+        $this->name=$name;
+        $this->hair_color=$color;
+        $this->type="貓";
+    }
+    public function hide(){
+        echo "很會躲";
+    }
+}
+class Dog extends Animal{
+    public function __construct($name,$color)
+    {
+        // parent::__construct($name,$color);
+        $this->name=$name;
+        $this->hair_color=$color;
+        $this->type="狗";
+    }
+    public function eat(){
+        echo "很會吃";
+    }
+}
 ?>
