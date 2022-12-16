dcaa9c6c44aeaba8835fbef52879471827481f17
ruzs <eddie10913@gmail.com>
2022-12-16 15:58:56 +0800

    建立一個用來存放各種資料庫操作方法的物件

---
+++ base.php
@@ -0,0 +1,70 @@
+<?php 
+
+
+class DB{
+    protected $table;
+    protected $dsn="mysql:host=localhost;charset=utf8;dbname=school";
+    protected $pdo;
+
+    public function __construct($table)
+    {
+        $this->pdo=new PDO($this->dsn,'root','');
+        $this->table=$table;
+    }
+
+
+    public function all(...$arg){
+/*
+  原本函式all()的內容
+global $pdo;
+    $sql="select * from $table ";
+    if(isset($args[0])){
+        if(is_array($args[0])){
+            //是陣列 ['acc'=>'mack','pw'=>'1234'];
+            //是陣列 ['product'=>'PC','price'=>'10000'];
+            foreach($args[0] as $key => $value){
+                $tmp[]="`$key`='$value'";
+            }
+            $sql=$sql ." WHERE ". join(" && " ,$tmp);
+        }else{
+            //是字串
+            $sql=$sql . $args[0];
+        }
+    }
+    if(isset($args[1])){
+        $sql = $sql . $args[1];
+    }
+    //echo $sql;
+    return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC); */
+
+    $sql="select * from $this->table ";
+
+    if(isset($args[0])){
+        if(is_array($args[0])){
+            //是陣列 ['acc'=>'mack','pw'=>'1234'];
+            //是陣列 ['product'=>'PC','price'=>'10000'];
+
+            foreach($args[0] as $key => $value){
+                $tmp[]="`$key`='$value'";
+            }
+
+            $sql=$sql ." WHERE ". join(" && " ,$tmp);
+        }else{
+            //是字串
+            $sql=$sql . $args[0];
+        }
+    }
+
+    if(isset($args[1])){
+        $sql = $sql . $args[1];
+    }
+
+    echo $sql;
+    return $this->pdo
+                ->query($sql)
+                ->fetchAll(PDO::FETCH_ASSOC);
+
+    }
+
+}
+?>
