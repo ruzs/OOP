ca2a42b9dd4758abebddb59010f67d2bc46a1488
mackliu <mackliu@wda.gov.tw>
2022-12-16 10:56:56 +0800

    實體化類別時帶入參數,同時更改類別的預設屬性內容

--- index.php
+++ index.php
@@ -25,23 +25,55 @@ echo "</pre>";
 
 //方法的使用
 
-$cat=new Animal;
+/* $cat=new Animal;
 $dog=new Animal;
 
 $cat->run();
 echo $cat->type;
-$cat->speed();
+$cat->speed(); */
 
+
+/* 建構式的使用
+*/
+$cat =new Animal('小花','黑白相間');
+echo $cat->getName();
+echo $cat->getColor();
+
+$dog =new Animal('小莉','土黃色');
+echo $dog->getName();
+echo $dog->getColor();
+
+$turtle =new Animal('達文西','墨綠色');
+echo $turtle->getName();
+echo $turtle->getColor();
+
+$guineapig =new Animal('puipui','黃色');
+echo $guineapig->getName();
+echo $guineapig->getColor();
 class Animal{
     protected $type='animal';
     protected $name='John';
     protected $hair_color="brown";
 
-    public function __constructor(){
-        //建構式內容
+    public function __construct($name,$color)
+    {
+        //$this->run();
+        $this->name=$name;
+        $this->hair_color=$color;
+
     }
 
-    public function run(){
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getColor()
+    {
+        return $this->hair_color;
+    }
+
+    public function run()
+    {
         echo "我會跑哦";
         $this->speed();
         echo $this->type;
