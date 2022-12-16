e9d3dfe6f1b06e244292bcaa1145135288e98bf6
mackliu <mackliu@wda.gov.tw>
2022-12-16 10:25:21 +0800

    物件中成員/屬性的使用方式

--- index.php
+++ index.php
@@ -1,6 +1,46 @@
 <?php
 
 
+/* $cat=new Animal;
+
+$dog=new Animal;
+
+echo $cat->type;
+echo "<br>";
+echo $dog->name; */
+
+/* 權限為public時，可以對成員做修改
+ $cat->type='snake';
+$dog->name='mack';
+echo "<br>";
+echo $cat->type;
+echo "<br>";
+echo $dog->name; */
+
+/* echo "<pre>";
+var_dump($cat);
+echo "</pre>";
+ */
+
+class Animal{
+    public $type='animal';
+    public $name='John';
+    public $hair_color="brown";
+
+    public function __constructor(){
+        //建構式內容
+    }
+
+    public function run(){
+        echo "我會跑哦";
+    }
+
+    private function speed(){
+        echo "我會叫哦";
+    }
+
+}
+
 
 
 ?>
